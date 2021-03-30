93ac7d64c11328c0c4b1b05f11eb814d221eb547
patil14 <patil.143@osu.edu>
2021-03-29 21:38:13 -0400

    updated firebase auth settings for gallery, reimported graphql schema.json

--- src/components/Gallery/Title.js
+++ src/components/Gallery/Title.js
@@ -5,6 +5,8 @@ const Title = () => {
     <div className="title">
       <h2>Gallery</h2>
       <p>Click to upload pictures</p>
+      <br></br>
+      <p style={{fontSize: "150%"}}>*Writing to gallery is only authorized to the owner*</p>
     </div>
   )
 }
