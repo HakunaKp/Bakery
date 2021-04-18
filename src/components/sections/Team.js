c15dffa65674941288e4708a0d89ecdfed74c7ef
patil14 <patil.143@osu.edu>
2021-04-17 23:10:59 -0500

    homepage

--- src/components/sections/Team.js
+++ src/components/sections/Team.js
@@ -2,7 +2,7 @@ import React from 'react';
 import classNames from 'classnames';
 import { SectionTilesProps } from '../../utils/SectionProps';
 import SectionHeader from './partials/SectionHeader';
-import Gallery from '.././gallery/Gallery';
+import Gallery from '../gallery/Gallery';
 
 const propTypes = {
   ...SectionTilesProps.types
