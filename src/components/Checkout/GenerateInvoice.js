af59734613b1404f0974cac31191bc2869e151e6
HakunaKp <hakunalmatata.kp@gmail.com>
2021-04-25 20:07:36 -0500

    minor pdf format change

--- src/components/Checkout/GenerateInvoice.js
+++ src/components/Checkout/GenerateInvoice.js
@@ -1,10 +1,16 @@
 import printTier from '../cart/PrintTier';
 
+function boolToWord(bool) {
+    if (bool) return "Yes";
+    else return "No";
+}
+
 export default function generateInvoiceTable(product, doc){
+
     doc.autoTable({
         head: [['Product ID', 'Flavor', 'Shape', 'Tier', 'Eggless', 'Fondant', 'Topper', 'Characters', 'Base Price']],
         body: [
-        [product[0].id, product[1].flavor, product[2].shape, printTier(product[3].tier), product[4].eggless, product[5].fondant, product[6].topper, product[7].characters, '$' + product[10].price],
+        [product[0].id, product[1].flavor, product[2].shape, printTier(product[3].tier), boolToWord(product[4].eggless), boolToWord(product[5].fondant), boolToWord(product[6].topper), boolToWord(product[7].characters), '$' + product[10].price],
         ]
     });
 
